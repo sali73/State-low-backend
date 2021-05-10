d7cb9e98b5223c9a93ab2bdb4ce54ff66d0e96a8
sali73 <sally.elgendy@hotmail.com>
2021-05-10 17:22:06 -0400

    change in schema

--- models/employee.js
+++ models/employee.js
@@ -7,7 +7,13 @@ const employeeSchema = Schema({
   name: { type: String, required: false },
   status: { type: String, default: false },
   legislative_process: { type: String, default: false },
-  members: [{ firstName: String, lastName: String }],
+  members: [
+    {
+      firstName: String,
+      lastName: String,
+      age: String,
+    },
+  ],
 });
 
 const Employee = model("employee", employeeSchema);
