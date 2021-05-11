80647516766c791fa4f51624b82486d0840af3c8
sali73 <sally.elgendy@hotmail.com>
2021-05-11 15:40:01 -0400

    add date

--- models/employee.js
+++ models/employee.js
@@ -7,7 +7,6 @@ const employeeSchema = Schema({
   name: { type: String, required: false },
   status: { type: String, default: false },
   legislative_process: { type: String, default: false },
-  lastActiveAt: { type: Date, default: false },
 });
 
 const Employee = model("employee", employeeSchema);
